7513fb3d40c6ab307b227d638a955e6708caa186
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-04-22 13:23:58 -0300

    Dia 2

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Welcome } from "./src/pages/Welcome";
+
+export default function App(){
+  return(
+    <Welcome />
+  )
+}
