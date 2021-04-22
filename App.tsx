2f4a0d9eee9a30208c3ac75bd7f51b04d1d15887
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-04-22 15:49:47 -0300

    Dia 2

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import { Welcome } from "./src/pages/Welcome";
+import { Confirmation } from "./src/pages/Confirmation";
+import { 
+  useFonts, 
+  Jost_400Regular, 
+  Jost_600SemiBold 
+} from "@expo-google-fonts/jost";
+import AppLoading from "expo-app-loading";
 
 export default function App(){
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return(
-    <Welcome />
+    <Confirmation />
   )
 }
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,22 +1,40 @@
 import React from "react";
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet, Dimensions, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
 
 import wateringImg from "../assets/watering.png" 
 import colors from '../styles/colors';
-import { Button } from '../components/Button';
+import fonts from '../styles/fonts';
 
 export function Welcome(){
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {"\n"}suas plantas {"\n"}de forma fácil
-            </Text>
-            <Image source={wateringImg} style={styles.image}/>
-            <Text style={styles.subtitle}>
-                  Não esqueça mais de regar suas plantas. 
-                  Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
-            <Button title=">"/>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {"\n"}suas plantas de{"\n"}forma fácil
+                </Text>
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image}
+                    resizeMode= "contain"
+                />
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas. 
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
+                <TouchableOpacity 
+                    style={styles.button}
+                    activeOpacity= {0.7}
+                >
+                
+                    <Feather 
+                        name="chevron-right"
+                        style={styles.buttonIcon} 
+                    />
+                
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -24,24 +42,43 @@ export function Welcome(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around',
+        paddingHorizontal: 17
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        margin: 38
+        margin: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
     },
     image: {
-        width: 292,
-        height: 284
+        height: Dimensions.get('window').width * 0.7     
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+    buttonIcon: {
+        fontSize: 32,
+        color: colors.white
     }
 });
